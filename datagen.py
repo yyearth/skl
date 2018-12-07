acaa9419290d288eb3d9bf9ac6bde972561923c7
yyearth <yyearth@163.com>
2018-12-07 11:01:56 +0800

    data generater

--- datagen.py
+++ datagen.py
@@ -1,21 +1,51 @@
 import numpy as np
 import cv2
-import random
-import requests
+import time
 
 
 # print(cv2.__version__)
 # print(np.__version__)
+# 3.4.3 cv
+# 1.15.1 numpy
+
+def linscale(d, range_):
+    """
+    Linear scaling.
+    :param d: data number in range between 0 to 1.
+    :param range_: scale in each dimension.
+    :return: scaled data.
+    """
+    # assert len(d) == len(range_)
+    m = np.array([])
+    a = np.array([])
+    for i, r in enumerate(range_):
+        if len(r) != 2:
+            continue
+        l, u = r
+        mul = (u - l) * np.ones((len(d), 1))
+        add = l * np.ones((len(d), 1))
+        if i == 0:
+            m = mul
+            a = add
+        else:
+            m = np.concatenate((m, mul), axis=1)
+            a = np.concatenate((a, add), axis=1)
+
+    return np.round(m * d + a, 1)
+
 
 class DataGen(object):
+    __tmpdata = []
+    WIDTH = 600
+    HEIGHT = 600
 
     def __init__(self, dim=2, data=None):
         self.dim = dim
         self.data = data
 
-    def gen(self, dim, n, range_=None):
+    def gen(self, n, dim, range_=None):
         """
-
+        Generate data.
         :param dim: dimension of data.
         :param n: num of sample data.
         :param range_: range in each dim. e.g. [[a, b], [c, d],...].
@@ -31,52 +61,65 @@ class DataGen(object):
 
         self.data = []
 
-        # for i in range(n):
-        #     one_sample = []
-        #     for r in range_:
-        #         d = random.uniform(*r)
-        #         # round(number[, ndigits]) -> number
-        #         da = round(d, 1)
-        #         one_sample.append(da)
-        #     self.data.append(one_sample)
-
         d = np.random.rand(n, dim)
-        m = np.array([])
-        a = np.array([])
-        for i, r in enumerate(range_):
-            l, u = r
-            mul = (u - l) * np.ones((n, 1))
-            add = u * np.ones((n, 1))
-            if i == 0:
-                m = mul
-                a = add
-            else:
-                m = np.concatenate((m, mul), axis=1)
-                a = np.concatenate((a, add), axis=1)
-        self.data = np.round(m * d + a, 1)
+        self.data = linscale(d, range_)
+
         return self.data
 
     @staticmethod
     def mouse(event, x, y, flags, param):
-        # print(x,y)
+        img, tmpdata = param
         if event == cv2.EVENT_LBUTTONDOWN:
-            print((x, y))
-            
+            tmpdata.append([x / DataGen.HEIGHT, (DataGen.WIDTH - y) / DataGen.WIDTH])
+            cv2.circle(img, (x, y), 2, (0, 0, 200), -1)
 
     def gen2d(self, n=None, range_=None):
+        """
+        Generate 2-dim data with cursor.
+        :param n: number of point.
+        :param range_: Range of X and Y.
+        :return: generated data.
+        """
 
+        if range_ is None:
+            range_ = [[0, 10]] * 2
+
+        assert len(range_) == 2, 'Length of param range_ must be 2.'
+
+        img = np.ones((DataGen.HEIGHT, DataGen.WIDTH, 3)) * 255
         cv2.namedWindow('DataGen')
-        cv2.setMouseCallback('DataGen', DataGen.mouse, self)
-        img = np.ones((600, 600)) * 255
+        cv2.setMouseCallback('DataGen', DataGen.mouse, (img, DataGen.__tmpdata))
 
         while True:
             cv2.imshow('DataGen', img)
             if cv2.waitKey(1) == 27:
                 break
+        cv2.destroyAllWindows()
+
+        d = np.array(DataGen.__tmpdata)
+        self.data = linscale(d, range_)
+
+        return self.data
+
+    def plot(self):
+        pass
+
+    def save(self, name=None):
+        if name is None:
+            name = './data{}.txt'.format(time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())))
+
+        np.savetxt(name, self.data)
+
+    def load(self):
+        pass
+
+    def dump(self, n):
+        pass
 
 
 if __name__ == '__main__':
     dg = DataGen()
-    # d = dg.gen(3, 4)
-    # print(d)
-    dg.gen2d()
+    d = dg.gen(4, 3, [[1, 2], [10, 20], [12, 13]])
+    # d = dg.gen2d()
+    print(d)
+    dg.save()
